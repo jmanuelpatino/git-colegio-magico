99b70dc691f8b891f5ecef44b2fbc9eaa2e288ee
juan manuel <juanpatinolondonocontact@gmail.com>
2026-09-05 15:49:26 +0000

    feat: resolvi el reto 1 (use el metodo invocar agregando mi nombre y el de mi profesor)

--- src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
+++ src/main/java/com/colegiomagico/reto1/HechizoIniciacion.java
@@ -6,7 +6,7 @@ package com.colegiomagico.reto1;
 public class HechizoIniciacion {
 
     public static String invocar() {
-        return ""; // TODO: Cambia esto
+        return "Mi nombre es \"Juan Manuel Patiño Londoño\" y el de mi profesor es \"Juan Jose\""; // TODO: Cambia esto
     }
 
     public static void main(String[] args) {
